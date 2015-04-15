e53a574b93fcba2c7f9f1c6e4ae7e045a4f5a5ad
Taneea <tannyaagrawal@gmail.com>
2015-04-15 21:54:24 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capita::Application.config.secret_key_base = 'fbf999d9633981acc14e1084b5a7f28be5556371380d0eab822c3fa31ac429a6ed9818ef458874f7dda3a0c1be21d2913147fcb4e217eb01a75093eaf2ab63cf'
